1421493e500a75548a81e7b527ea728227571a53
Benjamin Turner <benjamin@passionsplay.com>
2017-05-18 19:05:56 -0700

    Add initial plugin definition

---
+++ pp-intro-to-cmb2-example-fields.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The Passions Play Example CMB2 custom fields plugin bootstrap file
+ *
+ * Plugin Name:       Passions Play Example CMB2 custom fields
+ * Description:       A plugin to showcase registering custom fields using CMB2
+ * Version:           0.0.1
+ * Author:            Benjamin Turner
+ * Author URI:        http://passionsplay.com
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
